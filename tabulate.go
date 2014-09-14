77f18f51e077c23e87bfeb80c8e8d5f1e77cdd73
Vadim Kravcenko <bndrzz@gmail.com>
2014-09-14 12:35:20 +0300

    Normalize method

--- tabulate.go
+++ tabulate.go
@@ -1,29 +1,34 @@
 package main
 
 import "fmt"
+import "reflect"
 
 // Basic Structure of TableFormat
 type TableFormat struct {
-	LineTop []string
+	LineTop         []string
 	LineBelowHeader []string
 	LineBetweenRows []string
-	LineBottom []string
-	HeaderRow []string
-	Padding int
-	HeaderHide bool
-	FitScreen bool
+	LineBottom      []string
+	HeaderRow       []string
+	Padding         int
+	HeaderHide      bool
+	FitScreen       bool
 }
 
 var TableFormats map[string]TableFormat
 
 type Tabulate struct {
-	Data map[string]interface{}
-	Headers map[string]string
+	Data        []*TabulateRow
+	Headers     []string
 	FormatFloat string
 	TableFormat TableFormat
-	AlignNum string
-	AlignStr string
-	EmptyVar string
+	AlignNum    string
+	AlignStr    string
+	EmptyVar    string
+}
+
+type TabulateRow struct {
+	Elements []string
 }
 
 func (t *Tabulate) normalize() {
@@ -46,24 +51,59 @@ func (t *Tabulate) align() {
 
 }
 
-func (t *Tabulate) Render() {
-
+func (t *Tabulate) Render() string {
+	return ""
 }
 
-func (t *Tabulate) SetHeaders(headers map[string]string) {
-
+func (t *Tabulate) SetHeaders(headers map[string]string) *Tabulate {
+	return t
 }
 
-func (t *Tabulate) setFormatting(format string) {
-
+func (t *Tabulate) SetFormatting(format string) *Tabulate {
+	return t
 }
 
 func Create(data interface{}) *Tabulate {
-	return &Tabulate{Data:data.(map[string]interface{})}
+	t := &Tabulate{}
+
+	switch v := data.(type) {
+	case [][]interface{}, [][]string:
+		fmt.Println("2D array interface{}")
+	case [][]int32, [][]int64, [][]int:
+		fmt.Println("Int 64 2d Array")
+	case []string:
+		fmt.Println("String array")
+	case []int32, []int64, []int:
+		fmt.Println("Int 32 or int 64")
+	case []interface{}:
+		t.Data = normalize(data.([]interface{}))
+	default:
+		fmt.Println(v)
+	}
+
+	return t
 }
 
 func main() {
-	data := map[string]string{"wat":"wat"}
+	data := []interface{}{"test", "test", "test2"}
+	Create([]int{1, 2, 3, 4, 5})
+	Create([]string{"1", "2"})
+	Create([][]interface{}{{1, 2, 3}, {1, 2, 3}})
 	Create(data)
 	fmt.Println("waat")
+}
+
+func InterfaceSlice(slice interface{}) []interface{} {
+	s := reflect.ValueOf(slice)
+	if s.Kind() != reflect.Slice {
+		panic("InterfaceSlice() given a non-slice type")
+	}
+
+	ret := make([]interface{}, s.Len())
+
+	for i := 0; i < s.Len(); i++ {
+		ret[i] = s.Index(i).Interface()
+	}
+
+	return ret
 }
--- utils.go
+++ utils.go
@@ -1,11 +1,30 @@
 package main
 
-func normalizeArray() {
+import "fmt"
+import "strconv"
 
+func normalize(data []interface{}) []*TabulateRow {
+	rows := make([]*TabulateRow, len(data))
+	normalized := make([]string, len(data))
+
+	for index, element := range data {
+		switch v := element.(type) {
+		case int:
+			normalized = append(normalized, strconv.Itoa(element.(int)))
+		case int64:
+			normalized = append(normalized, strconv.FormatInt(element.(int64), 10))
+		case bool:
+			normalized = append(normalized, strconv.FormatBool(element.(bool)))
+		case float64:
+			normalized = append(normalized, strconv.FormatFloat(element.(float64), 'f', -1, 64))
+		case uint64:
+			normalized = append(normalized, strconv.FormatUint(element.(uint64), 10))
+		case []interface{}:
+			rows[index] = normalize(data)
+		default:
+			fmt.Println(v)
+		}
+	}
+
+	return []TabulateRow{}
 }
-
-func normalizeType() {
-
-}
-
-
