0fe1fd0269938e8218423b17c6cc5301d238fe1c
Vadim Kravcenko <bndrzz@gmail.com>
2014-09-19 11:12:45 +0300

    Add Tests structure

---
+++ tabulate_test.go
@@ -0,0 +1,88 @@
+package main
+
+import "fmt"
+import "testing"
+import "github.com/stretchr/testify/assert"
+
+var HEADERS = []string{"Header 1", "Header 2", "Header 3", "Header 4", "Header 5"}
+var INT_ARRAY = []int{1, 2, 3, 1000, 200}
+var INT64_ARRAY = []int64{100, 500, 600, 1000, 10000}
+var FLOAT_ARRAY = []float64{10.01, 12.002, -123.5, 20.00005, 1.01}
+var STRING_ARRAY = []string{"test string", "test string 2", "test", "row", "bndr"}
+var MIXED_ARRAY = []interface{}{"string", 1, 1.005, "another string", -2}
+
+// Test Setters
+func TestSetFormat(t *testing.T) {
+
+}
+
+func TestPadLeft(t *testing.T) {
+
+}
+
+func TestPadRight(t *testing.T) {
+
+}
+
+func TestPadCenter(t *testing.T) {
+
+}
+
+func TestSetHeaders(t *testing.T) {
+
+}
+
+func TestHeadersFirstRow(t *testing.T) {
+
+}
+
+func TestEmptyString(t *testing.T) {
+
+}
+
+func TestMaxColWidth(t *testing.T) {
+
+}
+
+// Test Simple
+func TestSimpleFloats(t *testing.T) {
+
+}
+
+func TestSimpleInts(t *testing.T) {
+
+}
+
+func TestSimpleString(t *testing.T) {
+
+}
+
+func TestSimpleMixed(t *testing.T) {
+
+}
+
+func TestSimpleMapMixed(t *testing.T) {
+
+}
+
+// Test Grid
+
+func TestGridFloats(t *testing.T) {
+
+}
+
+func TestGridInts(t *testing.T) {
+
+}
+
+func TestGridString(t *testing.T) {
+
+}
+
+func TestGridMixed(t *testing.T) {
+
+}
+
+func TestGridMapMixed(t *testing.T) {
+
+}
